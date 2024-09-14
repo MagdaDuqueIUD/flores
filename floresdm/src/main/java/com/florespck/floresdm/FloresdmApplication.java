dff189786033ee667775a371f3e760b085d2e605
MagdaDuqueIUD <maria.duquemr@est.iudigital.edu.co>
2024-09-14 09:13:51 -0500

    Configuración de la aplicación y mínimo producto viable

---
+++ floresdm/src/main/java/com/florespck/floresdm/FloresdmApplication.java
@@ -0,0 +1,14 @@
+package com.florespck.floresdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloresdmApplication.class, args);
+		System.out.println("APLICACIÓN INICIADA");
+	}
+
+}
---
+++ floresdm/src/test/java/com/florespck/floresdm/FloresdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florespck.floresdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloresdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
